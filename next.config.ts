9332aad2386c8dd1989b81baca0a62e8d65a2c58
keimei7 <shidashida7@icloud.com>
2026-03-27 16:02:16 +0900

    fix next standalone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    turbo: false,
+  },
 };
 
 export default nextConfig;
