823bc2315db6d9556ef1c56384f8da2bcc1ac5ea
keimei7 <shidashida7@icloud.com>
2026-04-02 00:48:16 +0900

    update header color from yellow to green for better brand consistency

--- app/reserve/page.tsx
+++ app/reserve/page.tsx
@@ -1013,7 +1013,7 @@ downloadCsv(filename, csv);
   </button>
 </div>
 
-          <div className="bg-yellow-300 border-b px-3 py-2.5 relative">
+          <div className="bg-gray-100 border-b px-3 py-2.5 relative">
             <div className="text-center font-bold text-xl">
             {currentTable?.title ?? "テーブル未選択"}
             </div>
